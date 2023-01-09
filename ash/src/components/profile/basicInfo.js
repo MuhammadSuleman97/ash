6a85c8be81b4feaff1de44e357fa8b0775c5ff69
Hafiz Awais <awaismahmood4@gmail.com>
2023-01-10 00:31:57 +0500

    basic tailwind setup and replace all css with tailwind css

--- ash/src/components/profile/basicInfo.js
+++ ash/src/components/profile/basicInfo.js
@@ -8,37 +8,55 @@ import './basicInfo.css'
 import persontick from '../../assets/person-tick.svg'
 import cover from '../../assets/cover.png'
 import profilePic from '../../assets/profile-pic.png'
+import buttonIcon from '../../assets/button-icon.png'
 
-export default function BasicInfo(){
+export default function BasicInfo() {
     return (
-        <div className='info'>
-            <div className='cover-pic'>
-                <img src={cover} />
+        <div className='w-full'>
+            <div className=''>
+                <img className='w-full' src={cover}/>
             </div>
-            <div className='info-detail'>
+            
+            <div className='bg-white flex justify-between px-5  items-center relative'>
 
-                <div className='information'>
-                    <div className='profile-pic'>
-                        <img src={profilePic} />
-                        </div>
-                    <div className='name'>Hafiz Awais</div>
-                    <div className='text-details'>
-                        <div className='text'>
-                            <img src={bucket}></img>
-                            <p>UX Designer</p>
-                        </div>
-                        <div className='text'>
-                        <img src={location}></img>
-                            <p>Vatican City</p>
-                        </div>
-                        <div className='text'>
-                        <img src={calender}></img>
-                            <p>Joined April 2011</p>
-                        </div>
+                <div className='flex items-center'>
+                    <div className='w-40 h-40 overflow-visible relative'>
+                        <img className='w-full absolute -top-7' src={profilePic}/>
+                    </div>
+                    <div className='ml-5 flex flex-col gap-5 pt-4 items-baseline'>
+                        <h2
+                            className='text-2xl'
+                            style={{
+                            "color": "rgba(50, 71, 92, 0.87)"
+                        }}>Hafiz Awais</h2>
+                        <div className='flex justify-between'>
+                            <div className='flex mr-6'>
+                                <img className='w-5 mr-3' src={bucket}></img>
+                                <p>UX Designer</p>
+                            </div>
+                            <div className='flex mr-6'>
+                                <img className='w-5 mr-3' src={location}></img>
+                                <p>Vatican City</p>
+                            </div>
+                            <div className='flex mr-6'>
+                                <img className='w-5 mr-3' src={calender}></img>
+                                <p>Joined April 2011</p>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
-                <div className='status-button'>ghjghjg</div>
+                <a
+                    className='flex justify-between items-center bg-indigo-700 text-white py-2 px-6 text-sm rounded-md mt-16'
+                    style={{
+                    "boxShadow": "0px 1px 6px 2px rgba(50, 71, 92, 0.06), 0px 2px 6px 1px rgba(50, 71, 92, 0.04), 0px 1px 4px 2px rgba(50, 71, 92, 0.02)"
+                }}
+                    href="/">
+                    <div className='w-4 mr-2'>
+                        <img className='w-full' src={buttonIcon}></img>
+                    </div>
+                    <p>CONNECTED</p>
+                </a>
             </div>
         </div>
     );
--- ash/src/components/profile/searchBar.js
+++ ash/src/components/profile/searchBar.js
@@ -11,19 +11,19 @@ import translate from '../../assets/Translate.svg'
 export default function SearchBar(){
 
     return (
-        <div className="header">
-            <div className='search-bar'>
-            
-            <div className='lang'><img src={search} /></div>
-            <input placeholder='Search (Ctrl+/)' ></input>
+        <div className="flex justify-between px-6 py-3 bg-white rounded-lg mb-6" style={{"boxShadow": "0px 1px 3px 2px rgba(50, 71, 92, 0.06), 0px 2px 5px 1px rgba(50, 71, 92, 0.04), 0px 1px 3px 2px rgba(50, 71, 92, 0.02)" }}>
+            <div className='w-full flex'>
+
+            <div className='w-6 h-6 ml-3'><img src={search} /></div>
+            <input className='border-0 ring-0 ml-5 w-full' placeholder='Search (Ctrl+/)' ></input>
 
             </div>
-            <div className='header-setting'>
-                <div className='lang'><img src={Vector1} /></div>
-                <div className='lang'><img src={grid} /></div>
-                <div className='lang'><img src={moon} /></div>
-                <div className='lang'><img src={wBadge} /></div>
-                <div className='lang'><img src={translate} /></div>
+            <div className='flex'>
+                <div className='w-6 h-6 ml-4'><img className='w-full' src={Vector1} /></div>
+                <div className='w-6 h-6 ml-4'><img className='w-full' src={grid} /></div>
+                <div className='w-6 h-6 ml-4'><img className='w-full' src={moon} /></div>
+                <div className='w-6 h-6 ml-4'><img className='w-full' src={wBadge} /></div>
+                <div className='w-6 h-6 ml-4'><img className='w-full' src={translate} /></div>
 
             </div>
         </div>
